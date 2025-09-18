d3f01bc561ed5316e8a9e11c409e7588e321f96d
Milo <JosephMRDominguez@gmail.com>
2025-09-18 04:10:18 -0700

    system-manager

---
+++ file-system-project/headers/system_manager.hpp
@@ -0,0 +1,31 @@
+#include "block.hpp"
+#include <disk_manager.hpp>
+#include "../utils/status_codes.cpp"
+#include "file_block.hpp"
+#include "root_block.hpp"
+
+#include <string>
+
+
+
+class SystemManager{
+    private:
+        DiskManager& _diskManager;
+        FileBlock* _lastOpened = nullptr;
+        char fileMode = 'N';
+        unsigned int _filePointer = 0;
+        RootBlock* _rootBlock = nullptr;
+
+        FileBlock* findFile(std::queue<std::string> nameBuffer);
+        SystemManager() = delete;
+    public:
+        SystemManager(DiskManager& diskManager, const std::string& rootName);
+        STATUS_CODE CREATE(const char& type, const std::string nameBuffer);
+        STATUS_CODE OPEN(const char& mode, const std::string nameBuffer);
+        STATUS_CODE CLOSE();
+        STATUS_CODE DELETE(const std::string nameBuffer);
+        std::pair<STATUS_CODE, std::string> READ(const int& numBytes);
+        STATUS_CODE WRITE(const int& numBytes, const std::string writeBuffer);
+        STATUS_CODE SEEK(const int& base, const int& offset);
+        ~SystemManager();
+};
